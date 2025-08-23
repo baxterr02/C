ef7c65efb2f054a9cee0cf28b3d79278153642e1
baxterr02 <bukhari.bakht@gmail.com>
2025-08-23 17:48:31 +0800

    Latest Commit

--- test.c
+++ test.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
 int main(){
+    // Learning C
+
+    /* I plan to learn Rust 
+    After learning C*/
     printf("\nHello World!");
+    return 0;
 }
---
+++ var.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    /*variable = A reusable container for a value.
+               Behaves as if it were the value it
+               contains */
+    int age = 23;
+    int year = 2025;
+    printf("\nYou are %d years old", age);
+    printf("\nThe year is %d", year); 
+    return 0;
+}
